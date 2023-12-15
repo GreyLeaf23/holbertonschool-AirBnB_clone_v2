172a90602f21c3261e0ebd6bffa23fab79ddeb6a
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-15 11:30:17 -0400

    Module sets route to display HTML page containing list of cities of states.

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""""Module set to load cities of states"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)  # Holds the name of the module.
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Method for closing Flask connection"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    Function returns a HTML page with a list
+    of states when routed to
+    """
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template('8-cities_by_states.html',
+                           states=states, cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
