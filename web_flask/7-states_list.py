ffa95ef5ea34ccd1acd1803927d082ecc4df5e9f
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-15 11:09:50 -0400

    Module sets a route that displays HTML page with list of states.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)  # Holds the name of the module.
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Function returns a HTML page with a list
+    of states when routed to
+    """
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """Method for closing Flask connection"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Runs the application.
